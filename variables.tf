7bf1ce9da2adb9ce0c4dd8870f44bf2487b7c984
Kevin Lefevre <lefevre.kevin@gmail.com>
2019-09-17 14:25:17 +0200

    initial commit

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "env" {}
+
+variable "aws" {
+  type    = any
+  default = {}
+}
+
+variable "registries" {
+  type    = any
+  default = {}
+}
+
+variable "custom_tags" {
+  type    = map
+  default = {}
+}
+
+variable "project" {
+  default = ""
+}
